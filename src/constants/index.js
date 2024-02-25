f027a967b7c84df2452927e1052ce0257d686be5
tinamai <tinatlmai@hotmail.com>
2024-02-25 00:24:45 -0500

    keyboard autoadjust elements done

--- src/constants/index.js
+++
@@ -1,4 +0,0 @@
-import { COLORS, SIZES, SHADOWS } from "./theme"
-import { dimensions, screen, fonts, inputArea, buttons } from "./styles"
-
-export { COLORS, SIZES, SHADOWS, dimensions, screen, fonts, inputArea, buttons }
